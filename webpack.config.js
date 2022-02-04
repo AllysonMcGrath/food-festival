0f350dd14fe8aad899d281b639fb019dd815631e
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-02-04 06:00:21 -0600

    add manifest webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 // main configuration object
 // entry is root of bundle, relative path to code
@@ -12,6 +13,13 @@ module.exports = {
         events: './assets/js/events.js',
         schedule: './assets/js/schedule.js',
         tickets: './assets/js/tickets.js'
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "./")
+      },
+      compress: true,
+      port: 8080,
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -48,6 +56,21 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", //the report outputs to an HTML file in the dist folder 
+        }),
+        new WebpackPwaManifest({
+          name: "Food Event",
+          short_name: "Foodies",
+          description: "An app that allows you to view upcoming food events.",
+          start_url: "../index.html",
+          background_color: "#01579b",
+          theme_color: "#ffffff",
+          fingerprints: false,
+          inject: false,
+          icons: [{
+            src: path.resolve("assets/img/icons/icon-512x512.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }]
         })
     ],
     mode: 'development'
